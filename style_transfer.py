199bfebc28ec8beff6f4887726931339e85e341e
Polina <testing6277@gmail.com>
2021-05-31 02:10:35 +0300

    minor changes

--- style_transfer.py
+++ style_transfer.py
@@ -8,7 +8,7 @@ import numpy as np
 import time
 
 
-# VISUALIZE THE INPUT
+# Подготовка данных
 def load_image(image_file):
     max_dim = 512
     img = Image.open(image_file)
@@ -17,13 +17,11 @@ def load_image(image_file):
     img = img.resize((round(img.size[0] * scale), round(img.size[1] * scale)), Image.LANCZOS)
 
     img = kp_image.img_to_array(img)
-    # We need to broadcast the image array such that it has a batch dimension
     img = np.expand_dims(img, axis=0)
 
     return img
 
 
-# PREPARE THE DATA
 def load_and_process_image(image_file):
     img = load_image(image_file)
     img = tf.keras.applications.vgg19.preprocess_input(img)
@@ -34,25 +32,27 @@ def deprocess_img(processed_img):
     x = processed_img.copy()
     if len(x.shape) == 4:
         x = np.squeeze(x, 0)
-    assert len(x.shape) == 3, ("Input to deprocess image must be an image of "
-                               "dimension [1, height, width, channel] or [height, width, channel]")
+    assert len(x.shape) == 3, ("Входные данные для преобразования изображения должны быть изображением типа "
+                               "измерение [1, высота, ширина, канал] или [высота, ширина, канал]")
     if len(x.shape) != 3:
-        raise ValueError("Invalid input to deprocessing image")
+        raise ValueError("Неверные входные данные для преобразования изображения")
 
-    # perform the inverse of the preprocessing step
+    # выполним операцию, обратную предварительной обработке
+    # изменение средних значений компонент
     x[:, :, 0] += 103.939
     x[:, :, 1] += 116.779
     x[:, :, 2] += 123.68
+
+    # Преобразование BGR -> RGB
     x = x[:, :, ::-1]
 
     x = np.clip(x, 0, 255).astype('uint8')
     return x
 
 
-# Content layer where will pull our feature maps
+# Слои содержания и стиля, с которых мы будем получать карты признаков
 content_layers = ['block5_conv2']
 
-# Style layer we are interested in
 style_layers = ['block1_conv1',
                 'block2_conv1',
                 'block3_conv1',
@@ -64,37 +64,29 @@ num_content_layers = len(content_layers)
 num_style_layers = len(style_layers)
 
 
-# BUILD THE MODEL
+# Построение модели
 def get_model():
-    """ Creates our model with access to intermediate layers.
-
-    This function will load the VGG19 model and access the intermediate layers.
-    These layers will then be used to create a new model that will take input image
-    and return the outputs from these intermediate layers from the VGG model.
-
-    Returns:
-      returns a keras model that takes image inputs and outputs the style and
-        content intermediate layers.
-    """
-    # Load our model. We load pretrained VGG, trained on imagenet data
+    # Загружаем предварительно обученную модель VGG19
     vgg = tf.keras.applications.vgg19.VGG19(include_top=False, weights='imagenet')
     vgg.trainable = False
-    # Get output layers corresponding to style and content layers
+    # Получим выходные слои, соответствующие слоям стиля и содержимого
     style_outputs = [vgg.get_layer(name).output for name in style_layers]
     content_outputs = [vgg.get_layer(name).output for name in content_layers]
     model_outputs = style_outputs + content_outputs
-    # Build model
+
     return models.Model(vgg.input, model_outputs)
 
 
-# COMPUTING CONTENT LOSS
+# Подсчет потерь содержимого
 def get_content_loss(base_content, target):
     return tf.reduce_mean(tf.square(base_content - target))
 
 
-# COMPUTING STYLE LOSS
+# Подсчет потерь стиля
+# Получение матрицы Грама
+
 def gram_matrix(input_tensor):
-    # We make the image channels first
+    # Вычислим число каналов
     channels = int(input_tensor.shape[-1])
     a = tf.reshape(input_tensor, [-1, channels])
     n = tf.shape(a)[0]
@@ -107,93 +99,57 @@ def get_style_loss(base_style, gram_target):
     return tf.reduce_mean(tf.square(gram_style - gram_target))
 
 
-# Apply style transfer to our images
-# Run Gradient Descent
+# Итоговые потери
+def compute_loss(model, loss_weights, init_image, content_features, gram_style_features):
+    content_weight, style_weight = loss_weights  # коэффициенты альфа и бета
 
-def get_feature_representations(model, content_img, style_img):
-    """Helper function to compute our content and style feature representations.
-
-    This function will simply load and preprocess both the content and style
-    images from their path. Then it will feed them through the network to obtain
-    the outputs of the intermediate layers.
-
-    Arguments:
-      model: The model that we are using.
-      content_path: The path to the content image.
-      style_path: The path to the style image
-
-    Returns:
-      returns the style features and the content features.
-    """
-    # Load our images in
-    content_image = load_and_process_image(content_img)
-    style_image = load_and_process_image(style_img)
-
-    # batch compute content and style features
-    style_outputs = model(style_image)
-    content_outputs = model(content_image)
-
-    # Get the style and content feature representations from our model
-    style_features = [style_layer[0] for style_layer in style_outputs[:num_style_layers]]
-    content_features = [content_layer[0] for content_layer in content_outputs[num_style_layers:]]
-    return style_features, content_features
-
-
-# Computing the loss and gradients
-def compute_loss(model, loss_weights, init_image, gram_style_features, content_features):
-    """This function will compute the loss total loss.
-
-    Arguments:
-      model: The model that will give us access to the intermediate layers
-      loss_weights: The weights of each contribution of each loss function.
-        (style weight, content weight, and total variation weight)
-      init_image: Our initial base image. This image is what we are updating with
-        our optimization process. We apply the gradients wrt the loss we are
-        calculating to this image.
-      gram_style_features: Precomputed gram matrices corresponding to the
-        defined style layers of interest.
-      content_features: Precomputed outputs from defined content layers of
-        interest.
-
-    Returns:
-      returns the total loss, style loss, content loss, and total variational loss
-    """
-    style_weight, content_weight = loss_weights
-
-    # Feed our init image through our model. This will give us the content and
-    # style representations at our desired layers. Since we're using eager
-    # our model is callable just like any other function!
+    # Пропускаем исходное изображение через нашу нейронную сеть
     model_outputs = model(init_image)
-
-    style_output_features = model_outputs[:num_style_layers]
+    # Выявляем необходимые признаки
     content_output_features = model_outputs[num_style_layers:]
+    style_output_features = model_outputs[:num_style_layers]
 
-    style_score = 0
+    # Величины потерь
     content_score = 0
+    style_score = 0
 
-    # Accumulate style losses from all layers
-    # Here, we equally weight each contribution of each loss layer
+    # Объединяем потери стиля со всех слоёв (формула 5)
     weight_per_style_layer = 1.0 / float(num_style_layers)
     for target_style, comb_style in zip(gram_style_features, style_output_features):
         style_score += weight_per_style_layer * get_style_loss(comb_style[0], target_style)
 
-    # Accumulate content losses from all layers
+    # Объединяем потери содержимого со всех слоёв (аналог формулы 5)
     weight_per_content_layer = 1.0 / float(num_content_layers)
     for target_content, comb_content in zip(content_features, content_output_features):
         content_score += weight_per_content_layer * get_content_loss(comb_content[0], target_content)
 
-    style_score *= style_weight
     content_score *= content_weight
+    style_score *= style_weight
 
-    # Get total loss
-    loss = style_score + content_score
-    return loss, style_score, content_score
+    # Получаем итоговые потери  # формула 6
+    loss = content_score + style_score
+    return loss, content_score, style_score
+
+
+# Полезные функции
+# Функция для получения карт признаков
+def get_feature_representations(model, content_img, style_img):
+    content_image = load_and_process_image(content_img)
+    style_image = load_and_process_image(style_img)
+
+    content_outputs = model(content_image)
+    style_outputs = model(style_image)
+
+    # Получим карты признаков стиля и содержания с помощью нашей модели
+    content_features = [content_layer[0] for content_layer in content_outputs[num_style_layers:]]
+    style_features = [style_layer[0] for style_layer in style_outputs[:num_style_layers]]
+    return content_features, style_features
 
 
 def compute_grads(cfg):
     with tf.GradientTape() as tape:
         all_loss = compute_loss(**cfg)
-    # Compute gradients wrt input image
+    # Вычисляем градиенты по входному изображению
     total_loss = all_loss[0]
     return tape.gradient(total_loss, cfg['init_image']), all_loss
 
@@ -201,35 +157,36 @@ def compute_grads(cfg):
 def run_style_transfer(content_img,
                        style_img,
                        num_iterations=1000,
-                       content_weight=1e3,
-                       style_weight=1e-2):
-    # We don't need to (or want to) train any layers of our model, so we set their
-    # trainable to false.
+                       content_weight=1e3, # альфа
+                       style_weight=1e-2): # бета
+
+    # Так как слои модели нам тренировать не нужно, поставим параметр trainable = False
     model = get_model()
     for layer in model.layers:
         layer.trainable = False
 
-    # Get the style and content feature representations (from our specified intermediate layers)
-    style_features, content_features = get_feature_representations(model, content_img, style_img)
+    # Получим карты признаков содержания и стиля с указанных выше слоёв
+    content_features, style_features = get_feature_representations(model, content_img, style_img)
+    # Посчитаем матрицы Грама для карт признаков стиля
     gram_style_features = [gram_matrix(style_feature) for style_feature in style_features]
 
-    # Set initial image
+    # Загрузим исходное изображение (создадим его копию для дальнейшего использования)
     init_image = load_and_process_image(content_img)
     init_image = tf.Variable(init_image, dtype=tf.float32)
-    # Create our optimizer
+
+    # Создадим оптимизатор для градиентного спуска
     opt = tf.optimizers.Adam(learning_rate=5, beta_1=0.99, epsilon=1e-1)
 
-    # Store our best result
+    # Переменные для сохранения наилучшего результата
     best_loss, best_img = float('inf'), None
 
-    # Create a nice config
-    loss_weights = (style_weight, content_weight)
+    loss_weights = (content_weight, style_weight)
     cfg = {
         'model': model,
         'loss_weights': loss_weights,
         'init_image': init_image,
-        'gram_style_features': gram_style_features,
-        'content_features': content_features
+        'content_features': content_features,
+        'gram_style_features': gram_style_features
     }
 
     norm_means = np.array([103.939, 116.779, 123.68])
@@ -247,13 +204,12 @@ def run_style_transfer(content_img,
 
     for i in range(num_iterations):
         grads, all_loss = compute_grads(cfg)
-        loss, style_score, content_score = all_loss
-        opt.apply_gradients([(grads, init_image)])
-        clipped = tf.clip_by_value(init_image, min_vals, max_vals)
+        loss, content_score, style_score = all_loss
+        opt.apply_gradients([(grads, init_image)])  # применяем вычисленный градиент к пикселям нашего изображения
+        clipped = tf.clip_by_value(init_image, min_vals, max_vals)  # ограничиваем пиксель изображения мин и макс знач
         init_image.assign(clipped)
 
         if loss < best_loss:
-            # Update best loss and best image from total loss.
             best_loss = loss
             best_img = deprocess_img(init_image.numpy())
 
